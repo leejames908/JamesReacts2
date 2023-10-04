d34a889e764adeea846d77ed0c5fa3812828c01d
unknown <lee.james908@gmail.com>
2023-10-03 20:40:03 -0400

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from '../src/App.js';
 
 
-/*
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,13 +14,7 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-*/
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+
 
 
 // If you want to start measuring performance in your app, pass a function
