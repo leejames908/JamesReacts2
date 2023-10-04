08cb64fa7bf2b0a24d88b4e2a8beb5aa79b436b2
unknown <lee.james908@gmail.com>
2023-10-03 20:21:12 -0400

    changed index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import About from './component/About.js';
 import { BrowserRouter } from "react-router-dom";
-import NavBar from '../src/component/NavBar.js';
 import App from '../src/App.js';
 
 
-
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -16,6 +14,14 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+*/
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
